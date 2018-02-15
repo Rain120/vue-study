d2c457fb55335be195e5fe0ccb53ad4c04b6d1e1
Rain120 <chao1202016@outlook.com>
2018-02-15 13:01:39 +0800

    init vue study demo 2018-2-15 1

---
+++ my-project/src/App.vue
@@ -0,0 +1,47 @@
+<template>
+  <div id="app">
+    <h1>在线翻译</h1>
+    <h5 class="text-muted">简单 / 易用 / 便捷</h5>
+    <translateForm v-on:formSubmit="translateText"></translateForm>
+    <translateOutput v-text="translatedText"></translateOutput>
+  </div>
+</template>
+
+<script>
+
+import TranslateForm from './components/TranslateForm'
+import TranslateOutput from './components/TranslateOutput'
+
+export default {
+  name: 'app',
+  components: {
+      TranslateForm,
+      TranslateOutput
+  },
+  data: function() {
+    return {
+      translatedText: ""
+    }
+  },
+  methods: {
+    translateText: function(text, language) {
+      this.$http.get('https://translate.yandex.net/api/v1.5/tr.json/translate?key=trnsl.1.1.20170929T110341Z.55bdd4c0d6e2af6d.43b4aec6bcf42c014f55519bfb6201d768302be9&lang='+ language + '&text=' + text)
+      .then(response => {
+          console.log(response.body.text[0]);
+          this.translatedText = response.body.text[0];
+      });
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
